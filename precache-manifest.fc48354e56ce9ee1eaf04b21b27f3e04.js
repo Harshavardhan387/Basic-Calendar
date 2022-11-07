bc64f9f72b4a8a81996cb12682e008e80a1505d1
Harshavardhan387 <harshadavidson96@gmail.com>
2022-11-07 13:21:06 +0530

    Updates

---
+++ precache-manifest.fc48354e56ce9ee1eaf04b21b27f3e04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "15977168a6020d424731",
+    "url": "/Basic-Calendar/static/css/main.2b67f42e.chunk.css"
+  },
+  {
+    "revision": "15977168a6020d424731",
+    "url": "/Basic-Calendar/static/js/main.15977168.chunk.js"
+  },
+  {
+    "revision": "80e4a5f900205f6a183a",
+    "url": "/Basic-Calendar/static/js/1.80e4a5f9.chunk.js"
+  },
+  {
+    "revision": "33876f90d72a50883907",
+    "url": "/Basic-Calendar/static/css/2.498d4be7.chunk.css"
+  },
+  {
+    "revision": "33876f90d72a50883907",
+    "url": "/Basic-Calendar/static/js/2.33876f90.chunk.js"
+  },
+  {
+    "revision": "32964c11e04b85990e12",
+    "url": "/Basic-Calendar/static/js/runtime~main.32964c11.js"
+  },
+  {
+    "revision": "16186d66827356c0a99c1518debbedd4",
+    "url": "/Basic-Calendar/index.html"
+  }
+];
